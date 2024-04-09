3a986f53e4ea0e75f16af800efb6e86537bef6a7
rajat <imrjtgupta@gmail.com>
2024-04-09 13:52:38 +0530

    called the OpenWeather Api for weather details

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
 import './App.css';
+import React,{ useState } from 'react'
 
 function App() {
+
+  const api = {
+    base:"https://api.openweathermap.org/data/2.5/weather",
+    key:"d37576e1cd5f576717bdeb2b436f21bf"
+  }
+
+  const [city,setCity] = useState("");
+  const [weather,setWeather] = useState({});
+    
+    function fetchData(){
+      const uri = `${api.base}?q=${city}&units=metric&appid=${api.key}`;
+      fetch(uri)
+      .then((response)=>{
+        response.json();
+      })
+      .then((data)=>{
+        setWeather(data);
+        console.log(data);
+      })
+    }
+
   return (
     <>
       <div className='container'>
@@ -9,13 +31,20 @@ function App() {
             <h1 className='heading'>Weather App</h1>
             <div className='inputArea'>
               <label htmlFor='city'>City Name : </label>
-              <input className='inputBox' id='city' placeholder="e.g. Ballia"></input>
-              <button className='btn'>Search</button>
+              <input className='inputBox' id='city' placeholder="e.g. Ballia" onChange={(e)=> setCity(e.target.value)}></input>
+              <button className='btn' onClick={fetchData}>Search</button>
             </div>
+{/* 
+            <div>
+              <p> { weather===undefined ? '' : weather.name} </p>
+              <p> { weather===undefined ? '' : weather.main.temp+" ℃"} </p>
+              <p> { weather===undefined ? '' : weather.weather[0].main} </p>
+            </div> */}
           </div>
       </div>
     </>
   );
 }
 
-export default App;
+export default App
+
